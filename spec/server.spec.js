a6da9911584f358bbff9af8733bd53cca569893c
headyt0pper <jstricklin89@gmail.com>
2019-03-25 15:41:47 -0400

    final final update

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -2,7 +2,7 @@ var request = require('request')
 
 describe('get homepage',()=>{
     it('should return 200 ok', (done) => {
-        request.get('http://4e384f4e.ngrok.io',(err,res)=>{ 
+        request.get('http://localhost:3000',(err,res)=>{ 
         expect(res.statusCode).toEqual(200)
             done()
         })
@@ -11,7 +11,7 @@ describe('get homepage',()=>{
 
 describe('get all database records',()=>{
     it('should return 200 ok', (done) => {
-        request.get('http://4e384f4e.ngrok.io/translations',(err,res)=>{ 
+        request.get('http://localhost:3000/translations',(err,res)=>{ 
         expect(res.statusCode).toEqual(200)
             done()
         })
@@ -20,7 +20,7 @@ describe('get all database records',()=>{
 
 describe('create a database entry',()=>{
     it('should return a success message', (done) => {
-        request.post('http://4e384f4e.ngrok.io/translations/test',(err,res)=>{ 
+        request.post('http://localhost:3000/translations/test',(err,res)=>{ 
         expect(res.statusCode).toEqual(200)
             done()
         })
